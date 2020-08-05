6094116b24955aca49ce5e991fd1b97c4ebedddb
Doug McClure <dmcclure@pagerduty.com>
2020-08-05 10:55:14 -0400

    pd app config

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const APP_CONFIG = {
-    redirectUrl: `https://pagerduty-samples.github.io/pagerduty-bulk-user-mgr-sample/`,
-    clientId: `37e14ac7da1930c2612f9b41ee69eea40087e317f020eb2b9336f4843c59927e`
+    redirectUrl: `https://dmcclure-pdt.github.io/aon/`,
+    clientId: `de88a2ec16e188c18b366917c9d0e704519d0bfc3a54122bfb49d9958fca7ba0`
 };
