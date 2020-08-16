189defe85426ff840f7f967a49f859e0ddd023ec
Doug McClure <dmcclure@pagerduty.com>
2020-08-16 08:06:11 -0400

    config, index

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 const APP_CONFIG = {
     //redirectUrl: `https://cbalan-pd.github.io/aon/`,
-    redirectUrl: `https://cbalan-ffb4e23d.localhost.run/`,
-    clientId: `2778b467995a8ad5224b08e0d2cdf8b9eff6e207faed5469019193da0ceb462f`
+    //redirectUrl: `https://cbalan-ffb4e23d.localhost.run/`,
+    redirectUrl: `https://dmcclure-pdt.github.io/aon/`,
+    clientId: `34f876cfed6c5285e0be87c59598b7bb0c0db60e0fa381d86aeec39d48f62fc9`
 };
