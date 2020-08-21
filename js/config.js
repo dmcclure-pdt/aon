0aa1770381f51c5479bdc7a0116baffd37ad06e2
cbalan-pd <68239651+cbalan-pd@users.noreply.github.com>
2020-08-20 22:53:59 -0400

    Update config.js
    cbalan website

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const APP_CONFIG = {
-    //redirectUrl: `https://cbalan-pd.github.io/aon/`,
+    redirectUrl: `https://cbalan-pd.github.io/aon/`,
     //redirectUrl: `https://cbalan-ffb4e23d.localhost.run/`,
-    redirectUrl: `https://dmcclure-pdt.github.io/aon/`,
+    //redirectUrl: `https://dmcclure-pdt.github.io/aon/`,
     clientId: `298d19b7da43294ca19decd3504dc8ca91369fde80c905ecb613097a829b2a39`
 };
