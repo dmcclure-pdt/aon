f0245da4c36088f07a7231dd72a0ba8232bf6a9a
cbalan-pd <68239651+cbalan-pd@users.noreply.github.com>
2020-08-20 22:42:59 -0400

    Updated configuration for GitHub Pages site

--- _site/js/config.js
+++ _site/js/config.js
@@ -1,5 +1,5 @@
 const APP_CONFIG = {
-    //redirectUrl: `https://cbalan-pd.github.io/aon/`,
-    redirectUrl: `https://cbalan-f9e06852.localhost.run/`,
+    redirectUrl: `https://cbalan-pd.github.io/aon/`,
+    //redirectUrl: `https://cbalan-f9e06852.localhost.run/`,
     clientId: `2778b467995a8ad5224b08e0d2cdf8b9eff6e207faed5469019193da0ceb462f`
 };
