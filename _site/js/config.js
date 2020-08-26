ae94628db6b848a19640d144bc5a1b982e2b388a
Doug McClure <dmcclure@pagerduty.com>
2020-08-26 08:48:00 -0400

    26-1

--- _site/js/config.js
+++ _site/js/config.js
@@ -1,6 +1,5 @@
 const APP_CONFIG = {
-    //redirectUrl: `https://cbalan-pd.github.io/aon/`,
-    redirectUrl: `https://cbalan-84e816a8.localhost.run/`,
+    redirectUrl: `https://dmcclure-pdt.github.io/aon/`,
     redirectStatusUpdateUrl: `https://xsgwlw98hl.execute-api.us-east-1.amazonaws.com/default/aon-demo`,
-    clientId: `2778b467995a8ad5224b08e0d2cdf8b9eff6e207faed5469019193da0ceb462f`
+    clientId: `de88a2ec16e188c18b366917c9d0e704519d0bfc3a54122bfb49d9958fca7ba0`
 };
--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const APP_CONFIG = {
     redirectUrl: `https://dmcclure-pdt.github.io/aon/`,
     redirectStatusUpdateUrl: `https://xsgwlw98hl.execute-api.us-east-1.amazonaws.com/default/aon-demo`,
-    clientId: `37e14ac7da1930c2612f9b41ee69eea40087e317f020eb2b9336f4843c59927e`
+    clientId: `de88a2ec16e188c18b366917c9d0e704519d0bfc3a54122bfb49d9958fca7ba0`
 };
