592cae3acd53a065691fff828e0f0a5e54b245c0
cbalan-pd <68239651+cbalan-pd@users.noreply.github.com>
2020-08-26 00:23:49 -0400

    first working version uses AWS GWY API

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 const APP_CONFIG = {
-    redirectUrl: `https://cbalan-pd.github.io/aon/`,
-    //redirectUrl: `https://cbalan-f9e06852.localhost.run/`,
+    //redirectUrl: `https://cbalan-pd.github.io/aon/`,
+    redirectUrl: `https://cbalan-84e816a8.localhost.run/`,
+    redirectStatusUpdateUrl: `https://xsgwlw98hl.execute-api.us-east-1.amazonaws.com/default/aon-demo`,
     clientId: `2778b467995a8ad5224b08e0d2cdf8b9eff6e207faed5469019193da0ceb462f`
 };
