f05178e18231089538dde71f31605688edfec6bd
cbalan-pd <68239651+cbalan-pd@users.noreply.github.com>
2020-09-01 18:24:06 -0400

    Initilal protoype

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const APP_CONFIG = {
-    redirectUrl: `https://cbalan-pd.github.io/aon/`,
-    //redirectUrl: `https://cbalan-b96eece8.localhost.run/`,
+    //redirectUrl: `https://cbalan-pd.github.io/aon/`,
+    redirectUrl: `https://cbalan-23aa97d9.localhost.run/`,
     redirectStatusUpdateUrl: `https://xsgwlw98hl.execute-api.us-east-1.amazonaws.com/default/aon-demo`,
     clientId: `2778b467995a8ad5224b08e0d2cdf8b9eff6e207faed5469019193da0ceb462f`
 };
